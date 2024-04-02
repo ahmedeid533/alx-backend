eeca330c4929326ecb80d5e066c7f6957e038642
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-04-02 12:07:34 +0200

    Basic dictionary with deffrient algorthims

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """Basic dictionary"""
 from base_caching import BaseCaching
-BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
+	"""BasicCache class"""
+
+	def __init__(self):
+		"""Initialize the class using the parent class __init__ method"""
+		BaseCaching.__init__(self)
+
 	def put(self, key, item):
+		"""Add an item in the cache"""
 		if key is None or item is None:
 			return
 		self.cache_data[key] = item
 
 	def get(self, key):
+		"""Get an item by key"""
 		if key is None or key not in self.cache_data:
 			return None
 		return self.cache_data[key]
